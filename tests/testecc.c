5b566de6d0af7d569c80f73c0dfad76035d3c201
Tony Cheneau <tony.cheneau@nist.gov>
2013-11-08 13:53:42 -0500

    Improve the ECC/ECDSA primitives test program

--- tests/testecc.c
+++ tests/testecc.c
@@ -1,19 +1,32 @@
-#include "certificate.h"
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "sha2.h"
+#include "ecdsa.h"
 #include "ecc.h"
 
 int main(int argc, char * argv[])
 {
-	s_certificate cert;
 	uint8_t hash[SHA256_DIGEST_LENGTH] = {0};
 	NN_DIGIT signature_r[NUMWORDS], signature_s[NUMWORDS];
 	NN_DIGIT privKey[NUMWORDS];
 	point_t pubKey;
+	uint8_t data [52] = {
+		1, 2, 3, 4, 5
+	};
+	SHA256_CTX ctx;
+
+	memset(privKey, 0, NUMBYTES);
+	memset(&pubKey, 0, sizeof(pubKey));
+	memset(signature_r, 0, NUMBYTES);
+	memset(signature_s, 0, NUMBYTES);
+
+
+	SHA256_Init(&ctx);
+	SHA256_Update(&ctx, data, 52);
+	SHA256_Final(hash, &ctx);
 
 	ecc_init();
 
@@ -24,19 +37,11 @@ int main(int argc, char * argv[])
 
 	ecdsa_sign(hash, signature_r, signature_s, privKey);
 
-	printf("%d\n",
-		   ecdsa_verify(hash, signature_r, signature_s, &pubKey)
-		  );
+	assert(ecdsa_verify(hash, signature_r, signature_s, &pubKey) == 1);
 
-	//generate_certificate(&cert);
+	signature_r[0] ^= 0xff;
 
-	//ecdsa_init(&cert.pub_cert.pub);
-
-	//ecdsa_sign(hash, signature_r, signature_s, cert.secret);
-
-	//printf("%d\n",
-	//       ecdsa_verify(hash, signature_r, signature_s, &cert.pub_cert.pub)
-	//      );
+	assert(ecdsa_verify(hash, signature_r, signature_s, &pubKey) != 1);
 
 	return 0;
 }
