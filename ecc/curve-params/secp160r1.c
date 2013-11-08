65dbf7ece7745e2fea2434aed7738e86df38c3d9
Tony Cheneau <tony.cheneau@nist.gov>
2013-11-08 17:44:49 -0500

    Make the SECP160R1 work

--- ecc/curve-params/secp160r1.c
+++ ecc/curve-params/secp160r1.c
@@ -4,10 +4,10 @@
 #define TRUE 1
 #define FALSE 0
 
-void 
+void
 get_curve_param(curve_params_t *para)
 {
-    
+
 #ifdef EIGHT_BIT_PROCESSOR
     //init parameters
     //prime
@@ -32,11 +32,11 @@ get_curve_param(curve_params_t *para)
     para->p[2] = 0xFF;
     para->p[1] = 0xFF;
     para->p[0] = 0xFF;
-    
+
     memset(para->omega, 0, NUMWORDS);
     para->omega[0] = 0x01;
     para->omega[3] = 0x80;
-    
+
     //cure that will be used
     //a
     para->E.a[20] = 0x00;
@@ -60,10 +60,10 @@ get_curve_param(curve_params_t *para)
     para->E.a[2] = 0xFF;
     para->E.a[1] = 0xFF;
     para->E.a[0] = 0xFC;
-	
+
     para->E.a_minus3 = TRUE;
     para->E.a_zero = FALSE;
-   
+
     //b
     para->E.b[20] = 0x00;
     para->E.b[19] = 0x1C;
@@ -86,7 +86,7 @@ get_curve_param(curve_params_t *para)
     para->E.b[2] = 0x65;
     para->E.b[1] = 0xFA;
     para->E.b[0] = 0x45;
-    
+
     //base point
     para->G.x[20] = 0x00;
     para->G.x[19] =  0x4A;
@@ -109,7 +109,7 @@ get_curve_param(curve_params_t *para)
     para->G.x[2] =  0xCB;
     para->G.x[1] =  0xFC;
     para->G.x[0] =  0x82;
-    
+
     para->G.y[20] = 0x00;
     para->G.y[19] =  0x23;
     para->G.y[18] =  0xA6;
@@ -131,7 +131,7 @@ get_curve_param(curve_params_t *para)
     para->G.y[2] =  0xC5;
     para->G.y[1] =  0xFB;
     para->G.y[0] =  0x32;
-    	
+
     //prime divide the number of points
     para->r[20] = 0x01;
     para->r[19] = 0x0;
@@ -154,9 +154,9 @@ get_curve_param(curve_params_t *para)
     para->r[2] = 0x75;
     para->r[1] = 0x22;
     para->r[0] = 0x57;
-     
+
     /* EIGHT_BIT_PROCESSOR */
-#elifdef SIXTEEN_BIT_PROCESSOR
+#elif defined(SIXTEEN_BIT_PROCESSOR)
 
     //init parameters
     //prime
@@ -170,10 +170,10 @@ get_curve_param(curve_params_t *para)
     para->p[2] = 0xFFFF;
     para->p[1] = 0x7FFF;
     para->p[0] = 0xFFFF;
-        
+
     para->omega[0] = 0x0001;
     para->omega[1] = 0x8000;
-    
+
     //cure that will be used
     //a
     para->E.a[9] = 0xFFFF;
@@ -186,10 +186,10 @@ get_curve_param(curve_params_t *para)
     para->E.a[2] = 0xFFFF;
     para->E.a[1] = 0x7FFF;
     para->E.a[0] = 0xFFFC;
-    	
+
     para->E.a_minus3 = TRUE;
     para->E.a_zero = FALSE;
-   
+
     //b
     para->E.b[9] = 0x1C97;
     para->E.b[8] = 0xBEFC;
@@ -201,7 +201,7 @@ get_curve_param(curve_params_t *para)
     para->E.b[2] = 0xD4AD;
     para->E.b[1] = 0xC565;
     para->E.b[0] = 0xFA45;
-        
+
     //base point
     para->G.x[9] =  0x4A96;
     para->G.x[8] =  0xB568;
@@ -224,7 +224,7 @@ get_curve_param(curve_params_t *para)
     para->G.y[2] =  0x5137;
     para->G.y[1] =  0x7AC5;
     para->G.y[0] =  0xFB32;
-        	
+
     //prime divide the number of points
     para->r[10] = 0x0001;
     para->r[9] = 0x0000;
@@ -237,9 +237,9 @@ get_curve_param(curve_params_t *para)
     para->r[2] = 0xAED3;
     para->r[1] = 0xCA75;
     para->r[0] = 0x2257;
-          
+
    /* SIXTEEN_BIT_PROCESSOR */
-#elifdef THIRTYTWO_BIT_PROCESSOR
+#elif defined(THIRTYTWO_BIT_PROCESSOR)
 
     //init parameters
     //prime
@@ -249,9 +249,9 @@ get_curve_param(curve_params_t *para)
     para->p[2] = 0xFFFFFFFF;
     para->p[1] = 0xFFFFFFFF;
     para->p[0] = 0x7FFFFFFF;
-    memset(para->omega, 0, NUMWORDS);        
+    memset(para->omega, 0, NUMWORDS);
     para->omega[0] = 0x80000001;
-    
+
     //cure that will be used
     //a
     para->E.a[5] = 0x00000000;
@@ -260,10 +260,10 @@ get_curve_param(curve_params_t *para)
     para->E.a[2] = 0xFFFFFFFF;
     para->E.a[1] = 0xFFFFFFFF;
     para->E.a[0] = 0x7FFFFFFC;
-    	
+
     para->E.a_minus3 = TRUE;
     para->E.a_zero = FALSE;
-   
+
     //b
     para->E.b[5] = 0x00000000;
     para->E.b[4] = 0x1C97BEFC;
@@ -271,7 +271,7 @@ get_curve_param(curve_params_t *para)
     para->E.b[2] = 0x65ACF89F;
     para->E.b[1] = 0x81D4D4AD;
     para->E.b[0] = 0xC565FA45;
-        
+
     //base point
     para->G.x[5] = 0x00000000;
     para->G.x[4] = 0x4A96B568;
@@ -286,7 +286,7 @@ get_curve_param(curve_params_t *para)
     para->G.y[2] = 0x59DCC912;
     para->G.y[1] = 0x04235137;
     para->G.y[0] = 0x7AC5FB32;
-        	
+
     //prime divide the number of points
     para->r[5] = 0x00000001;
     para->r[4] = 0x00000000;
@@ -298,181 +298,28 @@ get_curve_param(curve_params_t *para)
 #endif /* THIRTYTWO_BIT_PROCESSOR */
 
   }
-  
-NN_UINT 
+
+NN_UINT
 omega_mul(NN_DIGIT *a, NN_DIGIT *b, NN_DIGIT *omega, NN_UINT digits)
 {
-  
 #ifdef EIGHT_BIT_PROCESSOR
-
-#ifdef CPU_AVR
-    uint8_t n_d;
-    if (digits % 4 == 0)
-      n_d = digits/4;
-    else
-      n_d = digits/4 + 1;
-    
-    //r2~r10
-    //r11~r14
-    //r15 c[0]
-    //r16 i
-    //r17 c[3]
-    //r19 0
-    //r21:r20 b
-    //r23:r22 c
-    //r25 d
-    asm volatile (//"push r0 \n\t"
-		  "push r1 \n\t"
-		  "push r28 \n\t"
-		  "push r29 \n\t"
-		  "clr r2 \n\t"  //init 9 registers for accumulator
-		  "clr r3 \n\t"
-		  "clr r4 \n\t"
-		  "clr r5 \n\t"
-		  "clr r6 \n\t"
-		  "clr r7 \n\t"
-		  "clr r8 \n\t"
-		  "clr r9 \n\t"
-		  "clr r10 \n\t"  //end of init
-		  "clr r19 \n\t"  //zero
-		  "ldi r25, 4 \n\t"  //d=4
-		  "dec %3 \n\t"
-		  "ldi r16, 0 \n\t"  //i
-		  "movw r28, %A2 \n\t"  //load c
-		  "ld r15, Y \n\t"  //c[0]
-		  "adiw r28, 3 \n\t"
-		  "ld r17, Y \n\t"  //c[3]
-		  "OMEGA_LOOP1: mul r16, r25 \n\t"  //i*d
-		  "add r0, r25 \n\t"
-		  "movw r26, %A1 \n\t"
-		  "add r26, r0 \n\t"
-		  "adc r27, r1 \n\t"  //load b, (i-j+1)*d-1
-		  "movw r28, %A2 \n\t"  //load c
-		  "ld r14, -X \n\t"  //load b0~b(d-1)
-		  "ld r13, -X \n\t"
-		  "ld r12, -X \n\t"
-		  "ld r11, -X \n\t"
-		  "mul r11, r15 \n\t"  //t=0
-		  "add r2, r0 \n\t"
-		  "adc r3, r1 \n\t"
-		  "brcc OMEGA_T01 \n\t"
-		  "adc r4, r19 \n\t"
-		  "brcc OMEGA_T01 \n\t"
-		  "adc r5, r19 \n\t"
-		  "adc r6, r19 \n\t"
-		  "adc r7, r19 \n\t"
-		  "adc r8, r19 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T01: mul r12, r15 \n\t"  //t=1
-		  "add r3, r0 \n\t"
-		  "adc r4, r1 \n\t"
-		  "brcc OMEGA_T02 \n\t"
-		  "adc r5, r19 \n\t"
-		  "brcc OMEGA_T02 \n\t"
-		  "adc r6, r19 \n\t"
-		  "adc r7, r19 \n\t"
-		  "adc r8, r19 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T02: mul r13, r15 \n\t"  //t=2
-		  "add r4, r0 \n\t"
-		  "adc r5, r1 \n\t"
-		  "brcc OMEGA_T03 \n\t"
-		  "adc r6, r19 \n\t"
-		  "brcc OMEGA_T03 \n\t"
-		  "adc r7, r19 \n\t"
-		  "adc r8, r19 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T03: mul r14, r15 \n\t"  //t=3
-		  "add r5, r0 \n\t"
-		  "adc r6, r1 \n\t"
-		  "brcc OMEGA_T24 \n\t"
-		  "adc r7, r19 \n\t"
-		  "brcc OMEGA_T24 \n\t"
-		  "adc r8, r19 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T24: mul r11, r17 \n\t" //t=0, b0*c
-		  "add r5, r0 \n\t"
-		  "adc r6, r1 \n\t"
-		  "brcc OMEGA_T31 \n\t"
-		  "adc r7, r19 \n\t"
-		  "brcc OMEGA_T31 \n\t"
-		  "adc r8, r19 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T31: mul r12, r17 \n\t"  //t=1
-		  "add r6, r0 \n\t"
-		  "adc r7, r1 \n\t"
-		  "brcc OMEGA_T32 \n\t"
-		  "adc r8, r19 \n\t"
-		  "brcc OMEGA_T32 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T32: mul r13, r17 \n\t"  //t=2
-		  "add r7, r0 \n\t"
-		  "adc r8, r1 \n\t"
-		  "brcc OMEGA_T33 \n\t"
-		  "adc r9, r19 \n\t"
-		  "adc r10, r19 \n\t"
-		  "OMEGA_T33: mul r14, r17 \n\t"  //t=3
-		  "add r8, r0 \n\t"
-		  "adc r9, r1 \n\t"
-		  "adc r10, r19 \n\t"
-		  "st Z+, r2 \n\t"  //a[i*d] = r2
-		  "st Z+, r3 \n\t"
-		  "st Z+, r4 \n\t"
-		  "st Z+, r5 \n\t"
-		  "movw r2, r6 \n\t"  //can be speed up use movw
-		  "movw r4, r8 \n\t"
-		  "mov r6, r10 \n\t"  //can be remove
-		  "clr r7 \n\t"
-		  "clr r8 \n\t"
-		  "clr r9 \n\t"
-		  "clr r10 \n\t"
-		  "cp r16, %3 \n\t"  //i == 4?
-		  "breq OMEGA_LOOP1_EXIT \n\t"
-		  "inc r16 \n\t"
-		  "jmp OMEGA_LOOP1 \n\t"
-		  "OMEGA_LOOP1_EXIT: st Z+, r2 \n\t"
-		  "st Z+, r3 \n\t"
-		  "st Z+, r4 \n\t"
-		  "st Z+, r5 \n\t"
-		  "pop r29 \n\t"
-		  "pop r28 \n\t"
-		  "pop r1 \n\t"
-		  //"pop r0 \n\t"
-		  :
-		  :"z"(a),"a"(b),"a"(omega),"r"(n_d)
-		  :"r0","r1","r2","r3","r4","r5","r6","r7","r8","r9","r10","r11","r12","r13","r14","r15","r16","r17","r19","r25","r26","r27","r28","r29"
-		  );
-
-    return (digits+4);
-    /* CPU_AVR */
-#else
     //memset(a, 0, digits*NN_DIGIT_LEN);
     NN_Assign(a, b, digits);
     a[digits+3] += NN_AddDigitMult(&a[3], &a[3], omega[3], b, digits);
-    return (digits+4);    
-    
-#endif
-    /* EIGHT_BIT_PROCESSOR */  
-#elifdef SIXTEEN_BIT_PROCESSOR
 
+    return (digits+4);
+#endif
+
+#ifdef SIXTEEN_BIT_PROCESSOR
     //memset(a, 0, digits*NN_DIGIT_LEN);
     NN_Assign(a, b, digits);
     a[digits+1] += NN_AddDigitMult(&a[1], &a[1], omega[1], b, digits);
+
     return (digits+2);
+#endif
 
-    /* SIXTEEN_BIT_PROCESSOR */  
-#elifdef THIRTYTWO_BIT_PROCESSOR
-
-  NN_Assign(a, b, digits);
-  a[digits] += NN_AddDigitMult(&a[0], &a[0], omega[0], b, digits);
-  return (digits+1);
-  
-#endif /* THIRTYTWO_BIT_PROCESSOR */
-  
+#ifdef THIRTYTWO_BIT_PROCESSOR
+    a[digits] += NN_AddDigitMult(&a[0], &a[0], omega[0], b, digits);
+    return (digits+1);
+#endif
 }
